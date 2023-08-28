f20f466f782c83284247a9f874cfc6eedff40255
Emmanuel Ojeba <ojebasegun@gmail.com>
2023-08-28 08:20:43 +0100

    fix it

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <span>
-        <h1>Hi there come</h1>
+        <h1>Hi there!!!! this is the second</h1>
         <hr />
         <MarketingApp />
     </span>
