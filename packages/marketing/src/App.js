5495bb5fe576a4ce2517115fbe8730f58cf33bbf
Emmanuel Ojeba <ojebasegun@gmail.com>
2023-08-28 20:45:08 +0100

    added a generation

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
+
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/pricing' element={<Pricing />}></Route>
-                    <Route path='/' element={<Landing />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </StylesProvider>
-    </div>
+    return (
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/pricing' element={<Pricing />}></Route>
+                        <Route path='/' element={<Landing />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </StylesProvider>
+        </div>
+    );
 }
